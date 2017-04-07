be875f17c6277fa9de0e93c118a5ba4ebbbe0f5d
recuritis <recuritis@gmail.com>
2017-04-08 01:36:29 +0800

    scope-chains-closures

---
+++ scope-chains-closures/003-global-scope-and-shadowing.js
@@ -0,0 +1,7 @@
+function foo(){
+    var bar;
+    quux = '12123'
+    function zip(){
+        var quux;
+    }
+}
---
+++ scope-chains-closures/004-closures.js
@@ -0,0 +1,9 @@
+function foo(){
+    var bar;
+    quux = "32342";
+    function zip(){
+        var quux;
+        bar = true;
+    }
+    return zip;
+}
